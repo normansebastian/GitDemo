5f6a628c3ff17ebf9d9afe4c3f651de7ab722342
normansebastian <38253015+normansebastian@users.noreply.github.com>
2018-05-29 16:37:22 +0200

    Add Hello to new Demo class (brace yourself for Belzebub)

---
+++ GitDemo/Demo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo
+{
+    class Demo
+    {
+
+        static void Hello(string name) => Console.WriteLine($"Tjenis, hallå {name}!");
+    }
+}
